a6bdf74572e44a19a9e2f76c5ae01a5d2a843aab
Long Nguyen <nguyen.long.908132@gmail.com>
2023-11-04 14:42:16 +0700

    Properly parse multiline SMTP response

--- EmailClient.SmtpClient/SmtpClient.cs
+++ EmailClient.SmtpClient/SmtpClient.cs
@@ -1,19 +1,51 @@
+using System.Text;
+
 namespace EmailClient;
 
 public class SmtpClient
 {
+    private class SmtpResponse
+    {
+        public int Code { get; set; }
+        public List<string> Messages { get; } = new();
+        public override string ToString()
+        {
+            StringBuilder builder = new();
+            for (var i = 0; i < Messages.Count; ++i)
+            {
+                builder.Append(Code);
+                builder.Append(i == Messages.Count - 1 ? ' ' : '-');
+                builder.AppendLine(Messages[i]);
+            }
+            return builder.ToString();
+        }
+    }
     private readonly TextCommandClient _client;
     public SmtpClient(string host, ushort port)
     {
         _client = new(host, port);
     }
+    private async Task<SmtpResponse> ParseResponse()
+    {
+        SmtpResponse response = new();
+        string line;
+        do
+        {
+            line = await _client.ReceiveMessage();
+            response.Code = int.Parse(line.AsSpan(0, 3));
+            response.Messages.Add(line[4..]);
+        } while (line[3] != ' ');
+        return response;
+    }
     public async Task Connect()
     {
         await _client.Connect();
-        var serverInfo = await _client.ReceiveMessage();
+        var serverInfo = await ParseResponse();
         Console.Write(serverInfo);
+
         await _client.SendMessage("EHLO");
-        var greeting = await _client.ReceiveMessage();
+        var greeting = await ParseResponse();
         Console.Write(greeting);
     }
+    
 }
