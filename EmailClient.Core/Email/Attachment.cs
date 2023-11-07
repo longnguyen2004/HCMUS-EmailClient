6934180aa6f69c5f2a8a8f3fa2239dcf38059f4d
Long Nguyen <nguyen.long.908132@gmail.com>
2023-11-07 10:41:15 +0700

    Add `ToBase64` and `ToBytes` to interface

--- EmailClient.Core/Email/Attachment.cs
+++ EmailClient.Core/Email/Attachment.cs
@@ -6,6 +6,8 @@ namespace EmailClient;
 public interface IAttachment {
     string MimeType { get; }
     string FileName { get; }
+    public Stream ToBase64();
+    public Stream ToBytes();
 }
 
 public class AttachmentLocal: IAttachment {
@@ -19,22 +21,30 @@ public class AttachmentLocal: IAttachment {
     }
     public Stream ToBase64()
     {
-        return new CryptoStream(_file.OpenRead(), new ToBase64Transform(), CryptoStreamMode.Read);
+        return new CryptoStream(_file.Open(FileMode.Open, FileAccess.Read, FileShare.Read), new ToBase64Transform(), CryptoStreamMode.Read);
+    }
+    public Stream ToBytes()
+    {
+        return _file.Open(FileMode.Open, FileAccess.Read, FileShare.Read);
     }
 }
 
 public class AttachmentRemote: IAttachment {
-    private readonly MemoryStream _b64Stream;
+    private readonly byte[] _b64Bytes;
     public string FileName { get; private set; }
     public string MimeType { get; private set; }
     public AttachmentRemote(string base64, string fileName, string mimeType)
     {
-        _b64Stream = new(Encoding.ASCII.GetBytes(base64));
+        _b64Bytes = Encoding.ASCII.GetBytes(base64);
         FileName = fileName;
         MimeType = mimeType;
     }
+    public Stream ToBase64()
+    {
+        return new MemoryStream(_b64Bytes);
+    }
     public Stream ToBytes()
     {
-        return new CryptoStream(_b64Stream, new FromBase64Transform(), CryptoStreamMode.Read);
+        return new CryptoStream(new MemoryStream(_b64Bytes), new FromBase64Transform(), CryptoStreamMode.Read);
     }
 }
