6309d6bb59c258b1b3faf31ce967bb5e06e043b8
kijen28 <phamquyen28112004@gmail.com>
2023-11-08 17:14:43 +0700

    Broke the main function

--- EmailClient.CommandLine/Program.cs
+++ EmailClient.CommandLine/Program.cs
@@ -2,11 +2,4 @@
 using EmailClient;
 
 Console.WriteLine("Hello, World!");
-
-using (FileStream stream = new("./test.msg", FileMode.Open, FileAccess.Read))
-using (StreamReader reader = new(stream))
-{
-    MimeParser parser = new(reader);
-    var parsed = await parser.Parse();
-}
-Console.ReadLine();
+Console.WriteLine("Minh nhat ga vcl");
