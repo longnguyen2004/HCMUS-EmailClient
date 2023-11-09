0957ce57d8fcb3fa37bdb4d98fe1cfda9044eec1
Long Nguyen <nguyen.long.908132@gmail.com>
2023-11-09 19:32:41 +0700

    Add `ToMime` function, make the 3 email lists settable

--- EmailClient.Core/Email/Email.cs
+++ EmailClient.Core/Email/Email.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.RegularExpressions;
 using MCollections;
 
@@ -10,9 +11,9 @@ public partial class Email
     )]
     public static partial Regex EmailAddressRegex();
     public string? From { get; set; }
-    public IndexedSet<string> To { get; } = new();
-    public IndexedSet<string> Cc { get; } = new();
-    public IndexedSet<string> Bcc { get; } = new();
+    public IndexedSet<string> To { get; set; } = new();
+    public IndexedSet<string> Cc { get; set; } = new();
+    public IndexedSet<string> Bcc { get; set; } = new();
     public string? Subject { get; set; }
     public string? Body { get; set; }
     public string? HtmlBody { get; set; }
@@ -22,7 +23,7 @@ public partial class Email
         foreach (var match in EmailAddressRegex().Matches(emails).Cast<Match>())
             list.Add(match.Value);
     }
-    public Email() {}
+    public Email() { }
     public Email(MimeEntity mime)
     {
         if (mime.Headers.TryGetValue("From", out var from))
@@ -77,5 +78,65 @@ public partial class Email
                 }
             }
         }
+    }
+    public MimeEntity ToMime()
+    {
+        static MimePart createBodyMime(string body, string contentType)
+        {
+            Dictionary<string, MimeHeaderValue> header = new()
+            {
+                {
+                    "Content-Type",
+                    new(contentType, new(){
+                        {"charset", "UTF-8"}
+                    })
+                }
+            };
+            var utf8Bytes = Encoding.UTF8.GetBytes(body);
+            // Not pure ASCII, need encoding
+            if (utf8Bytes.Length != body.Length)
+            {
+                body = Convert.ToBase64String(utf8Bytes);
+                header["Content-Transfer-Encoding"] = new("base64");
+            }
+            return new MimePart(header, body);
+
+        }
+        MimeEntity message;
+        var textPart = createBodyMime(Body ?? "", "text/plain");
+        if (HtmlBody != null)
+        {
+            var htmlPart = createBodyMime(HtmlBody, "text/html");
+            message = new MimeMultipart(
+                new(),
+                new(){ textPart, htmlPart },
+                "",
+                "alternative"
+            );
+        }
+        else
+        {
+            message = textPart;
+        }
+        if (Attachments.Count > 0)
+        {
+            var withAttachments = new MimeMultipart(
+                new(),
+                new(){ message },
+                "This is a multi-part message in MIME format.\r\n"
+            );
+            foreach (var attachment in Attachments)
+                withAttachments.Parts.Add(new MimeAttachment(attachment));
+            message = withAttachments;
+        }
+        if (From != null)
+            message.Headers["From"] = new(From);
+        if (To.Count > 0)
+            message.Headers["To"] = new(string.Join(", ", To));
+        if (Cc.Count > 0)
+            message.Headers["Cc"] = new(string.Join(", ", Cc));
+        if (Subject != null)
+            message.Headers["Subject"] = new(Subject);
+        return message;
     }
 }
