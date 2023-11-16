49c2bc29d24abdc07fd8eb6ab092a375731b2504
Long Nguyen <nguyen.long.908132@gmail.com>
2023-11-16 19:01:51 +0700

    Initial GUI work

---
+++ EmailClient.Gui/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EmailClient.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailClient.Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly EmailContext _context = new("test.db");
+        private CollectionViewSource emailCollectionViewSource;
+        public MainWindow()
+        {
+            InitializeComponent();
+            emailCollectionViewSource = (CollectionViewSource)FindResource(nameof(emailCollectionViewSource));
+        }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() => {
+                _context.Database.Migrate();
+                _context.Emails.Load();
+            });
+            emailCollectionViewSource.Source = _context.Emails.Local.ToObservableCollection();
+        }
+    }
+}
